6a992073cda507f6a83c4b8fa3290c81294d9126
GenuineGenius999 <lm1876356@gmail.com>
2025-12-15 17:58:52 +0900

    fixed: solved the bug

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Snowfall from '@/components/Snowfall';
 import HeroSection from '@/components/HeroSection';
 import PhotoGallery from '@/components/PhotoGallery';
 import MessageSection from '@/components/MessageSection';
-import AudioPlayer from '@/components/AudioPlayer';
+// import AudioPlayer from '@/components/AudioPlayer';
 import Footer from '@/components/Footer';
 import FloatingGifts from '@/components/FloatingGifts';
 import SparkleStars from '@/components/SparkleStars';
@@ -60,14 +60,14 @@ const Index = () => {
       </motion.div>
 
       {/* Audio Player Section */}
-      <motion.section
+      {/* <motion.section
         className="py-12 md:py-16 px-4 flex justify-center"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true }}
       >
         <AudioPlayer audioUrl={audioUrl} />
-      </motion.section>
+      </motion.section> */}
 
       {/* Message Section */}
       <MessageSection />
